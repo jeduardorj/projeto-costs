2ab10e7c5d5ee9ec718ea80cd6df9f9c488e04f4
eduoliveirarj <104588175+jeduardorj@users.noreply.github.com>
2022-12-27 17:03:54 -0300

    inclusão de alguns componentes e de css

--- costs/src/App.js
+++ costs/src/App.js
@@ -1,31 +1,40 @@
-import { BrowserRouter as Router, Routes, Route,Link} from 'react-router-dom'
+import { BrowserRouter as Router,Routes,Route,Link } from "react-router-dom";
 import Home from './components/pages/Home';
 import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
 import NewProject from './components/pages/NewProject';
 
+import Container from "./components/layout/Container";
+
+
 function App() {
   return (
+    
     <Router>
-      <ul>
+      <div>
         <Link to="/">Home</Link>
-        <li>Contato</li>
-      </ul>
-      
-      <Routes>
-        <Route exact path='/'>
-          <Home/>
-        </Route>
-        <Route exact path='/company'>
-          <Company/>
-        </Route>
-        <Route exact path='/contact'>
-          <Contact/>
-        </Route>
-        <Route exact path='/newproject'>
-          <NewProject/>
-        </Route>
-      </Routes>
+        <Link to="/contact">Contato</Link>
+        <Link to="/company">Empresa</Link>
+        <Link to="/newproject">Novo Projeto</Link>
+      </div>
+    <Container customClass="min-height">
+        <Routes>
+          <Route exact path='/' element={<Home/>}></Route>
+        </Routes>
+
+        <Routes>
+          <Route exact path='/company' element={<Company/>}></Route>
+        </Routes>
+
+        <Routes>
+          <Route exact path='/contact' element={<Contact/>}></Route>
+        </Routes>
+
+        <Routes>
+          <Route exact path='/newproject' element={<NewProject/>}></Route>
+          
+        </Routes>
+      </Container>
       <p>Footer</p>
     </Router>
   );
