a3fc48cd1a2c3f51be3dfbad8ead6ddc3a1ab102
eduoliveirarj <104588175+jeduardorj@users.noreply.github.com>
2022-12-27 15:51:51 -0300

    Envio de codigo

--- costs/src/App.js
+++ costs/src/App.js
@@ -1,9 +1,33 @@
+import { BrowserRouter as Router, Routes, Route,Link} from 'react-router-dom'
+import Home from './components/pages/Home';
+import Company from './components/pages/Company';
+import Contact from './components/pages/Contact';
+import NewProject from './components/pages/NewProject';
 
 function App() {
   return (
-    <div className="App">
-     <p>Costs</p>
-    </div>
+    <Router>
+      <ul>
+        <Link to="/">Home</Link>
+        <li>Contato</li>
+      </ul>
+      
+      <Routes>
+        <Route exact path='/'>
+          <Home/>
+        </Route>
+        <Route exact path='/company'>
+          <Company/>
+        </Route>
+        <Route exact path='/contact'>
+          <Contact/>
+        </Route>
+        <Route exact path='/newproject'>
+          <NewProject/>
+        </Route>
+      </Routes>
+      <p>Footer</p>
+    </Router>
   );
 }
 
